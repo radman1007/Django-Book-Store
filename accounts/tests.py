2120687b81f6c197881eba70d3c911bb1feb07fb
radman montazer <radman@radmans-MacBook-Pro.local>
2024-06-25 13:53:31 +0330

    write test for apps

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class SignUpPageTest(TestCase):
+    def test_signup_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        
+    def Test_signup_url(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_signup_form(self):
+        user = get_user_model().objects.create_user(
+            'myusername',
+            'myusername@gmail.com',
+        )
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, 'myusername')
+        self.assertEqual(get_user_model().objects.all()[0].email, 'myusername@gmail.com')
