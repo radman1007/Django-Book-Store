730b631e3ad0a908ef8e714743f0f2aee5bb8c80
radman1007 <radman.cod110@gmail.com>
2024-06-29 15:22:14 +0330

    add email backend

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,6 @@ LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.Email.Backend'
