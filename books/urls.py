498c16dd06241a020421a1e81dd8106093be7256
radman1007 <radman.cod110@gmail.com>
2024-07-01 00:59:25 +0330

    add url

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import BookListView, BookDetailView, BookCreateView
+from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book_list'),
     path('<int:pk>/', BookDetailView.as_view(), name='book_detail'),
-    path('book-create/', BookCreateView.as_view(), name='book_create'),
+    path('new/', BookCreateView.as_view(), name='book_create'),
+    path('<int:pk>/edit/', BookUpdateView.as_view(), name='book_update'),
 ]
