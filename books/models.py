cee37f150817e6ce448f790fc37a46fc4c29458d
radman1007 <radman.cod110@gmail.com>
2024-07-01 00:16:33 +0330

    get absolute url

--- books/models.py
+++ books/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.db import models
 
 class Book(models.Model):
@@ -8,3 +9,7 @@ class Book(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("book_detail", args={self.id})
+    
