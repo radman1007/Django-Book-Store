c2840d76e12c4895631c7cfd65c60e78fb2a1ea8
radman1007 <radman.cod110@gmail.com>
2024-07-01 00:02:35 +0330

    detail view in view

--- books/views.py
+++ books/views.py
@@ -5,3 +5,8 @@ class BookListView(generic.ListView):
     model = Book
     template_name = 'book_list.html'
     context_object_name = 'books'
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'
