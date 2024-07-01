bd4e57ca54c21299f9cddcc2d9c9cf85632d3f98
radman1007 <radman.cod110@gmail.com>
2024-07-01 13:41:26 +0330

    change code to upper

--- books/templatetags/book_tags.py
+++ books/templatetags/book_tags.py
@@ -3,5 +3,5 @@ from django import template
 register = template.Library()
 
 @register.filter
-def to_lowercase(value):
-    return value.lower()
+def to_lowercase(value, arg):
+    return f'{arg}: {value.upper()}'
