f1b784640f31c373dbd9b344a16a6e9c96786c78
radman1007 <radman.cod110@gmail.com>
2024-07-03 17:47:54 +0330

    add list display

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Book, Comment
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'text' , 'datetime_created')
+
 admin.site.register(Book)
 admin.site.register(Comment)
