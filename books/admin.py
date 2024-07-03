39a6866754b03bd1885fe8834fbe8462bfe189d0
radman1007 <radman.cod110@gmail.com>
2024-07-03 19:36:14 +0330

    add bookadmin

--- books/admin.py
+++ books/admin.py
@@ -5,5 +5,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('user', 'book', 'text', 'recommend', 'datetime_created')
     
 
-admin.site.register(Book)
 admin.site.register(Comment, CommentAdmin)
+admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'price')
