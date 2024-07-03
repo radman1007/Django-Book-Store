bee3ff0b85b8cecd94e05d43561f529903a667e3
radman1007 <radman.cod110@gmail.com>
2024-07-03 19:33:16 +0330

    add recommend in admin page

--- books/admin.py
+++ books/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Book, Comment
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'book', 'text' , 'datetime_created')
+    list_display = ('user', 'book', 'text', 'recommend', 'datetime_created')
     
 
 admin.site.register(Book)
