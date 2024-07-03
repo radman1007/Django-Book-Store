2d3cc10afc281b62d655648dd2c7e6ae67360ce3
radman1007 <radman.cod110@gmail.com>
2024-07-03 17:52:18 +0330

    migrate

--- books/models.py
+++ books/models.py
@@ -20,7 +20,7 @@ class Comment(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     text = models.TextField()
-    datetime_creates = models.DateTimeField(auto_now_add=True)
+    datetime_created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.text
