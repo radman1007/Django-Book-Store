cae64bae4aef63111f8aa2c5e6b4a9cc800a251f
radman1007 <radman.cod110@gmail.com>
2024-07-04 11:30:12 +0330

    add book form

--- books/forms.py
+++ books/forms.py
@@ -5,3 +5,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('text', 'recommend',)
+        
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ('title', 'author', 'description', 'price', 'cover',)
